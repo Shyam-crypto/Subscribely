52603c5fa6cf562d33103d084d88d043b6317bad
punithshetty321 <puni0692@gmail.com>
2023-11-06 11:42:21 +0530

    sly-7,8

---
+++ client/components/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      // Send a POST request to your backend for user authentication
+      const response = await axios.post('/api/login', {
+        email,
+        password,
+      });
+
+      // Handle success (e.g., redirect to the dashboard)
+      console.log(response.data);
+    } catch (error) {
+      // Handle errors (e.g., display an error message)
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
--- client/pages/App.js
+++ client/pages/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Login from '../components/Login';
+import Login from  '../components/Login'
 import Register from '../components/Register';
 
-const Home = () => {
+function App() {
   return (
     <div>
       <Login />
       <Register />
     </div>
   );
-};
+}
 
-export default Home;
+export default App;
