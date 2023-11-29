1a12d303f532ecc8e7f524b852951a8eb2b41e68
punithshetty321 <puni0692@gmail.com>
2023-11-29 16:58:19 +0530

    login and register

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,47 +1,45 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Login = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Sending post request
-    const response = await fetch('/api/User', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (response.ok) {
-      // Redirect after login
-      window.location.href = '';
-    } else {
-      // Handle login error
-      const data = await response.json();
-      setError(data.error);
-    }
+  const handleLogin = () => {
+    
+    router.push('/');
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Login</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
+      <form>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
       </form>
+
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
+      `}</style>
     </div>
   );
 };
--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -1,52 +1,45 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Register = () => {
-  const [name, setName] = useState('');
+  const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Send a POST request to server for user registration
-    const response = await fetch('/api/Auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-
-    if (response.ok) {
-      // Registration successful, redirect the user to a login page 
-      window.location.href = '/login'; 
-    } else {
-      // Handle registration error
-      const data = await response.json();
-      setError(data.error);
-    }
+  const handleRegister = () => {
+  
+    router.push('/');
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Register</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Register</button>
+      <form>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <br />
+        <button type="button" onClick={handleRegister}>
+          Register
+        </button>
       </form>
+
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
+      `}</style>
     </div>
   );
 };
